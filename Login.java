059048985472ca4ea744f968282b8ca46581ebfd
kumarsandeep100 <sandeepk0054@gmail.com>
2026-02-08 11:22:06 -0500

    Initial commit: add login module and project setup

---
+++ Login.java
@@ -0,0 +1,40 @@
+import java.awt.Image;
+
+import javax.swing.*;
+import java.awt.*;
+
+class Login extends JFrame{
+
+  Login() {
+
+    setTitle("AuTOMATIC TELLER MACNINE");
+
+    setLayout(null);
+
+    ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg"));
+    Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+    ImageIcon i3 = new ImageIcon(i2);
+    JLabel label = new JLabel(i3);
+    label.setBounds(70,10,100,100);
+    add(label);
+
+    JLabel text = new JLabel("Welocme to ATM");
+    text.setFont(new Font("Osward",Font.BOLD,38));
+    text.setBounds(200,40,400,40);
+    add(text);
+
+    getContentPane().setBackground(Color.WHITE);
+
+    setSize(800,480);
+    setVisible(true);
+    setLocation(350,200);
+
+
+  }
+  public static void main(String[] args) {
+
+    new Login();
+    
+  }
+  
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+\# ATM Simulator System 🚧
+
+
+
+A desktop-based banking application built using Java Swing, AWT, and MySQL.
+
+
+
+Currently working on login and authentication.
+
+
+
+\## Technologies
+
+\- Java (Swing, AWT)
+
+\- MySQL
+
+\- JDBC
+
+
+
+\## Status
+
+In Progress 🚧
+
+
+
+\## Author
+
+Sandeep Kumar
+
+
+
